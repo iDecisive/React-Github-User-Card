e673bdeaa7d8aee90cd829ccb78ce1456e3a8841
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-06-02 17:11:04 -0400

    Changed Appjs to class component and added card component

---
+++ github-user-card/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import Card from './components/Card';
+
+import './App.css';
+
+
+class App extends React.Component {
+
+  render() {
+  return (
+    <div className="App">
+
+    <Card />
+
+    </div>
+  );
+  }
+}
+
+export default App;
---
+++ github-user-card/src/components/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Card extends React.Component {
+
+    constructor(props) {
+        super();
+
+    }
+
+    render() {
+
+        return (
+
+            <h1>hi</h1>
+
+        )
+
+    }
+
+}
+
+export default Card;
